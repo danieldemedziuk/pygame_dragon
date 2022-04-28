33c42e52a9509ed9158d09e652e5ad3b448ff5de
Daniel Demedziuk <daniel.demedziuk@gmail.com>
2022-04-28 19:02:20 +0200

    added exp system and exp stats

--- code/level.py
+++ code/level.py
@@ -90,6 +90,7 @@ class Level:
                                     self.obstacle_sprites,
                                     self.damage_player,
                                     self.trigger_death_particles,
+                                    self.add_exp,
                                 )
 
     def create_attack(self):
@@ -131,6 +132,9 @@ class Level:
 
     def trigger_death_particles(self, pos, particle_type):
         self.animation_player.create_particles(particle_type, pos, [self.visible_sprites])
+
+    def add_exp(self, amount):
+        self.player.exp += amount
 
     def run(self):
         # update and draw the game
