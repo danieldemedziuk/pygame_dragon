edfbbeccaabed96d8fcdd1ef3507806690c49d37
Daniel Demedziuk <daniel.demedziuk@gmail.com>
2022-04-29 17:02:28 +0200

    change main music volume

--- code/main.py
+++ code/main.py
@@ -18,6 +18,7 @@ class Game:
 
         # sound
         main_sound = pygame.mixer.Sound('../audio/main.ogg')
+        main_sound.set_volume(0.5)
         main_sound.play(loops=-1)
 
     def run(self):
